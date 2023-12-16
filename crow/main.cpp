407bdbbaa11e6498b353d4de163c0c3837165b20
karoltomaszewski <karoltomaszewskiit@gmail.com>
2023-12-16 15:48:02 +0100

    Delete comments

--- crow/main.cpp
+++ crow/main.cpp
@@ -1,42 +1,11 @@
 ﻿#include "FEN.h"
 #include "Engine.h"
 #include <iostream>
-/*int main()
-{
 
-    crow::SimpleApp app; //define your crow application
-
-    //define your endpoint at the root directory
-    CROW_ROUTE(app, "/")([]() {
-        return std::to_string(7*7);
-    });
-
-    CROW_ROUTE(app, "/post").methods("POST"_method)([&app](const crow::request& req) {
-        std::cout << req.body;
-        auto x = crow::json::load(req.body);
-
-        if (!x)
-            return crow::response(crow::status::BAD_REQUEST);
-
-        std::string f = std::string(x["fen"]);
-
-        engine::Engine engine(f);
-
-        engine::Engine::bestMoveStructure s = engine.findBestMove();
-
-        return crow::response(s.notation + " : " + std::to_string(s.evaluation));
-    });
-
-    //set the port, set the app to run on multiple threads, and run the app
-    app.port(2000).multithreaded().run();
-}
-*/
 int main(int argc, char* argv[])
 {
-    //std::string fen = "r1bq1rk1/p2nnppp/4p3/1Bpp2N1/8/2N1P3/PPP2PPP/R2QK2R w KQ - 2 10";
     std::string fen = argv[1];
     std::string drawPositions = argv[2];
-    //std::string drawPositions = "";
 
     engine::Engine engine(fen, drawPositions);
 
